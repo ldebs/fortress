85f7fa4ddfd519d5b4ffcc96a7756a6f493e7408
ldebat <ldebat@hotmail.com>
2021-06-08 03:08:07 +0200

    Serial param + smooth + cmnd status

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,18 @@
 #include <ArduinoJson.h>
 
 // ----------------- delays
-#define DELAY 100
-#define PRINT_TIMEOUT (10 * 1000)
+#define DELAY 5
+#define PRINT_TIMEOUT (5 * 60 * 1000l)
+
+#ifndef SERIAL_SPEED
+#warning "SERIAL_SPEED not defined"
+#define SERIAL_SPEED 9600
+#endif
+
+#ifndef SERIAL_CONFIG
+#warning "SERIAL_CONFIG not defined"
+#define SERIAL_CONFIG SERIAL_8N1
+#endif
 
 // ----------------- PINS
 // OUTPUTS
@@ -17,29 +27,31 @@
 
 const int inputPins[] = {POWER_PIN, BATTERY_PIN, OVERLOAD_PIN};
 
+// STATUS
 #define FRONT_FLAG 0x2
 #define FRONT_MASK (~FRONT_FLAG)
-int states[] = {LOW, LOW, LOW};
-unsigned long lastMillis = 0;
-bool hasToPrint = true;
 
+#define OFF LOW
+#define ON HIGH
+#define DOWN (FRONT_FLAG & ON)
+#define UP (FRONT_FLAG & ON)
+#define NB_STATE_VAL 4
+
+// STATES
+#define STATE_POWER 0
+#define STATE_BATTERY 1
+#define STATE_OVERLOAD 2
 #define NB_STATE 3
-#define NB_STATE_VAL 4;
-const char *JSON_FIELDS[] = {
-    // {
-    "sensor", // = sensor value
-    "status", //=
-    // {
-    "power",    // = one of status values
-    "battery",  // = one of status values
-    "overload", // = one of status values
-    // },
-    "time" // = currentMillis
-           // }
-};
+
+#define NB_HISTO (1000 / DELAY)
+
+int statesHisto[NB_HISTO][NB_STATE];
+int states[NB_STATE];
+int statePointer;
+unsigned long lastMillis;
+bool hasToPrint;
+
 const char *VALUES_STR[] = {
-    // sensor
-    "fortress 900 v2",
     // status
     "OFF",  // already off
     "ON",   // already on
@@ -47,10 +59,36 @@ const char *VALUES_STR[] = {
     "UP"    // just change to on
 };
 
+#define PPCAT(a, b) a##b
+#define JSON_BEGIN Serial.print("{\"sensor\":\"Fortress 900 v2\",")
+#define JSON_OBJ(o) Serial.print(#o ":{")
+#define JSON_KV(k, v) Serial.print(#k ":");Serial.print("\"");Serial.print(v);Serial.print("\"")
+#define JSON_Kv(k, v) Serial.print(#k ":");Serial.print(v)
+#define JSON_COMMA Serial.print(",")
+#define JSON_ENDOBJ Serial.print("}")
+#define JSON_END Serial.println("}")
+#define JSON_RESULT(v) Serial.println("{\"sensor\":\"Fortress 900 v2\",\"RESULT\":" #v "}")
+
 void setup()
 {
+  // init state historic
+  for (int h = 0; h < NB_HISTO; h++)
+  {
+    for (int s = 0; s < NB_STATE; s++)
+    {
+      statesHisto[h][s] = OFF;
+    }
+  }
+  statePointer = 0;
+  lastMillis = 0;
+  hasToPrint = true;
+
   // initialize serial communication
-  Serial.begin(9600);
+  Serial.begin(SERIAL_SPEED, SERIAL_CONFIG);
+  while (!Serial)
+  {
+    ; // wait for serial port to connect. Needed for native USB
+  }
   // make pins inputs:
   pinMode(POWER_PIN, INPUT_PULLUP);
   pinMode(BATTERY_PIN, INPUT_PULLUP);
@@ -63,6 +101,8 @@ void setup()
   // A3 as VCC
   pinMode(VCC, OUTPUT);
   digitalWrite(VCC, HIGH);
+
+  JSON_RESULT("setup complete");
 }
 
 void loop()
@@ -70,20 +110,57 @@ void loop()
   unsigned long currentMillis = millis();
 
   // read the input pins
-  for (int i = 0; i < NB_STATE; i++)
+  for (int s = 0; s < NB_STATE; s++)
   {
-    int curState = HIGH - digitalRead(inputPins[i]);
-    // detect front up/down
-    if (curState != (states[i] & FRONT_MASK))
+    int curState = HIGH - digitalRead(inputPins[s]);
+    statesHisto[statePointer][s] = curState;
+
+    if (statePointer % NB_HISTO / 2 == 0)
     {
-      // set front flag
-      states[i] = FRONT_FLAG | curState;
-      hasToPrint = true;
+      // compute mean state
+      curState = OFF;
+      for (int h = 0; h < NB_HISTO; h++)
+      {
+        curState |= statesHisto[h][s];
+      }
+
+      // detect front up/down
+      if (curState != (states[s] & FRONT_MASK))
+      {
+        // set front flag
+        states[s] = FRONT_FLAG | curState;
+        hasToPrint = true;
+      }
+      else
+      {
+        // remove front flag
+        states[s] = curState;
+      }
+    }
+  }
+
+  if (Serial.available())
+  {
+    // Serial.println("read...");
+    // String str=Serial.readString();
+    StaticJsonDocument<128> doc;
+    DeserializationError err = deserializeJson(doc, Serial);
+    if (!err)
+    {
+      const char *cmnd = doc["cmnd"] | "";
+      if (strcmp(cmnd, "status") == 0)
+      {
+        hasToPrint = true;
+        JSON_RESULT("OK");
+      }
+      else
+      {
+        JSON_RESULT("Unknown command");
+      }
     }
     else
     {
-      // remove front flag
-      states[i] = curState;
+      JSON_RESULT("Bad syntax");
     }
   }
 
@@ -100,21 +177,20 @@ void loop()
   // serial output (JSON) if there is any change or PRINT_TIMEOUT
   if (hasToPrint)
   {
-    int f = 0, v = 0;
-    StaticJsonDocument<128> doc;
-    doc[JSON_FIELDS[f++]] = VALUES_STR[v++];
-    JsonObject state = doc.createNestedObject(JSON_FIELDS[f++]);
-    for (int i = 0; i < NB_STATE; i++)
-    {
-      state[JSON_FIELDS[f++]] = VALUES_STR[v + states[i]];
-    }
-    v += NB_STATE_VAL;
-    doc[JSON_FIELDS[f++]] = currentMillis;
-
-    serializeJson(doc, Serial);
-    Serial.println();
+    JSON_BEGIN;
+    JSON_OBJ("status");
+    JSON_KV("power",VALUES_STR[states[STATE_POWER]]);
+    JSON_COMMA;
+    JSON_KV("battery",VALUES_STR[states[STATE_BATTERY]]);
+    JSON_COMMA;
+    JSON_KV("overload",VALUES_STR[states[STATE_OVERLOAD]]);
+    JSON_ENDOBJ;
+    JSON_COMMA;
+    JSON_Kv("time",currentMillis);
+    JSON_END;
   }
 
   delay(DELAY);
   hasToPrint = false;
+  statePointer = (statePointer + 1) % NB_HISTO;
 }
